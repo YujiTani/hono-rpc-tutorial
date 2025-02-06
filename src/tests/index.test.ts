8374d71527758abbd0fe934ace7de334dc40bb67
Yuji Tani <valleyyuji.webengineer@gmail.com>
2025-02-06 21:33:54 +0900

    404エラー時の処理を追加

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,10 +1,16 @@
 import { describe, expect, it } from 'bun:test'
-import app from '.'
+import app from '..'
 
 describe('My first test', () => {
   it('Should return 200 Response', async () => {
     const req = new Request('http://localhost/')
     const res = await app.fetch(req)
     expect(res.status).toBe(200)
+  })
+
+  it('Should return 404 Response', async () => {
+    const req = new Request('http://localhost/not-found')
+    const res = await app.fetch(req)
+    expect(res.status).toBe(404)
   })
 })
