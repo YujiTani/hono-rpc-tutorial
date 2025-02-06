7e097792cf696ac9231a2bcf62034a10a585dfc3
Yuji Tani <valleyyuji.webengineer@gmail.com>
2025-02-07 00:20:24 +0900

    openapiを使った処理に書き換え中

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,8 @@
-import { Hono } from 'hono'
 import { serveStatic } from 'hono/bun'
-import { z } from 'zod'
 import { zValidator } from '@hono/zod-validator'
+import { createRoute, OpenAPIHono, z } from '@hono/zod-openapi'
 
-const app = new Hono()
+const app = new OpenAPIHono()
 
 app.use('/static/*', serveStatic({ root: './' }))
 app.use('/favicon.ico', serveStatic({ path: './favicon.ico' }))
@@ -16,14 +15,35 @@ const users = [
   { id: '2', name: 'Jane', age: 21 },
 ]
 
+// パラメータの型定義
 const reqIdSchema = z.object({
-  id: z.string(),
-})
+  id: z
+  .string()
+  .openapi({
+    param: {
+      name: 'id',
+      description: 'ユーザーのID',
+      in: 'path',
+    },
+    example: '999999',
+  }),
+}).openapi('reqIdSchema')
 
+// ユーザーを作成するための型定義
 const reqUserCreateSchema = z.object({
-  name: z.string(),
-  age: z.number(),
-})
+  name: z
+  .string()
+  .openapi({
+    description: 'ユーザーの名前',
+    example: 'tarou',
+  }),
+  age: z
+  .number()
+  .openapi({
+    description: 'ユーザーの年齢',
+    example: 20,
+  }),
+}).openapi('reqUserCreateSchema')
 
 /**
  * ユーザーを取得
@@ -31,23 +51,47 @@ const reqUserCreateSchema = z.object({
  * @returns ユーザーの情報
  */
 app
-.get('/api/users/:id',
-  zValidator('param', reqIdSchema),
-  (c) => {
-  const { id } = c.req.valid('param')
-  const user = users.find((user) => user.id === id)
-
-  if (!user) {
-    return c.json(
-      {
-        error: 'not found'
+.openapi(
+  createRoute({
+    method: 'get',
+    path: '/api/users/:id',
+    request: {
+      params: reqIdSchema,
+    },
+    responses: {
+      200: {
+        content: {
+          'application/json': {
+            schema: z.object({
+              id: z.string(),
+              name: z.string(),
+              age: z.number(),
+            }),
+          },
+        },
+        description: 'ユーザーの情報',
       },
-      404
-    )
-  }
-
-  return c.json(user, 200)
-})
+    },
+  }),
+  async (c) => {
+    console.log('c', c)
+    
+    const { id } = c.req.valid("param");
+    const user = users.find((user) => user.id === id)
+    
+    // if (!user) {
+    //   return c.json(
+    //     {
+    //       error: 'not found'
+    //     },
+    //     404
+    //   )
+    // }
+    
+    return c.json(user, 200)
+  },
+  zValidator('param', reqIdSchema),
+)
 
 /**
  * ユーザーを作成
